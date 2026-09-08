2a97327cbbe0eebb63b3e5236f66e2759d762c82
Your Name <yourmail@example.org>
2026-09-08 10:50:29 +0200

    install python

---
+++ parameters.R
@@ -0,0 +1,6 @@
+# PARAMETETRS PROJECT
+root = "F:/PhD/De Alleluia/data/raw_data"
+ms_se_path = "sf_mss_okt_25.xlsx"
+colls_path = "collections.xlsx"
+
+root_out = "F:/PhD/De Alleluia/data/deriv_data"
